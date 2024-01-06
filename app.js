582fc6d52501bb1c5d0bb54935b7525bc02cd89f
Mudasser Rasool <86182941+MudassserRasool@users.noreply.github.com>
2024-01-06 17:56:49 +0500

    hjsd sad

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-
-
 import express from 'express';
 import mongoose from 'mongoose';
-import orderRoutes from './routes/order.js';
+import workoutRoutes from './routes/workout.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://mern2022:mern2022@ac-1vocr4t-shard-00-00.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-01.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-02.43aaypx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x4e2nh-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  'mongodb://mern2022:mern2022@ac-1vocr4t-shard-00-00.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-01.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-02.43aaypx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x4e2nh-shard-0&authSource=admin&retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error('MongoDB connection error:', error));
 db.once('open', () => console.log('Connected to MongoDB'));
 
-app.use('/api', orderRoutes);
+app.use('/api/workouts', workoutRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
